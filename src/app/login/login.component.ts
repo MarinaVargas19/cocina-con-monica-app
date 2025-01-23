8a1dcaa8efbfd9a75a457ef8594a630135374d67
MarinaVargas19 <m.vcampos.2019@gmail.com>
2025-01-23 20:16:04 +0100

    implemented login component 3

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+ import { FormsModule } from '@angular/forms';
+  import { CommonModule } from '@angular/common';
 
-@Component({
-  selector: 'app-login',
-  standalone: true,
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
-export class LoginComponent {
+ @Component({
+   selector: 'app-login',
+    standalone: true,
+    imports: [FormsModule, CommonModule],
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+ })
+export class LoginComponent {
+  username = '';
+  password = '';
 
-}
+  login() {
+   console.log('Usuario:', this.username);
+    console.log('Contraseña:', this.password);
+   // Aquí iría la lógica de autenticación
+ }
+ }
