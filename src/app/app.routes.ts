04d8f398c642c842554ff8e4435b67b7a67fd30d
MarinaVargas19 <m.vcampos.2019@gmail.com>
2025-01-23 18:53:26 +0100

    route login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,3 +5,13 @@ export const routes: Routes = [
 { path: '', redirectTo: '/welcome', pathMatch: 'full' },
  { path: 'welcome', component: WelcomeComponent }
 ];
+
+import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+   { path: 'welcome', component: WelcomeComponent },
+    { path: 'login', component: LoginComponent }
+];
